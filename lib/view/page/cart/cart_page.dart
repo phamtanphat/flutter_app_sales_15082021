42b4eac2f3b8ac4da10c146f0fe8104c78e7d1a4
phamtanphat <phatdroid94@gmail.com>
2021-11-17 21:04:27 +0700

    update cart page

---
+++ lib/view/page/cart/cart_page.dart
@@ -0,0 +1,194 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_sales_15082021/base/base_widget.dart';
+import 'package:flutter_app_sales_15082021/model/cart_model.dart';
+import 'package:flutter_app_sales_15082021/model/food_model.dart';
+import 'package:flutter_app_sales_15082021/repository/order_repository.dart';
+import 'package:flutter_app_sales_15082021/request/order_request.dart';
+import 'package:flutter_app_sales_15082021/view/page/cart/cart_bloc.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+
+import 'cart_event.dart';
+
+class CartPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PageContainer(
+      child: CartPageContainer(),
+      providers: [
+        Provider(create: (context) => OrderRequest()),
+        ProxyProvider<OrderRequest, OrderRepository>(
+          create: (context) => OrderRepository(),
+          update: (context, request, repository) {
+            repository!.updateOrderRequest(request);
+            return repository;
+          },
+        ),
+        ChangeNotifierProxyProvider<OrderRepository, CartBloc>(
+          create: (context) => CartBloc(),
+          update: (context, orderRepository, bloc) {
+            bloc!.updateOrderRepo(orderRepository);
+            return bloc;
+          },
+        )
+      ],
+      appBar: AppBar(
+        title: Text("Cart"),
+      ),
+    );
+  }
+}
+
+class CartPageContainer extends StatefulWidget {
+  const CartPageContainer({Key? key}) : super(key: key);
+
+  @override
+  _CartPageContainerState createState() => _CartPageContainerState();
+}
+
+class _CartPageContainerState extends State<CartPageContainer> {
+  late CartBloc bloc;
+  late String orderId;
+
+  @override
+  void didChangeDependencies() {
+    orderId = (ModalRoute.of(context)!.settings.arguments as Map)['orderId'];
+
+    bloc = context.read();
+    bloc.eventSink.add(CartEventOrderDetail());
+
+    bloc.resultController.stream.listen((event) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(event.toString())));
+    });
+    super.didChangeDependencies();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamProvider.value(
+      value: bloc.cartController.stream,
+      initialData: null,
+      child: Consumer<CartModel>(
+        builder: (context, cartModel, child) {
+          if (cartModel == null) {
+            return SizedBox();
+          }
+          return Container(
+            child: Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                      itemCount: cartModel.items!.length,
+                      itemBuilder: (lstContext, index) =>
+                          _buildItem(cartModel.items![index], context)),
+                ),
+                Container(
+                    margin: EdgeInsets.symmetric(vertical: 10),
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        color: Colors.teal,
+                        borderRadius: BorderRadius.all(Radius.circular(5))),
+                    child: Text(
+                        "Tổng tiền : " +
+                            NumberFormat("#,###", "en_US")
+                                .format(cartModel.total) +
+                            " đ",
+                        style: TextStyle(fontSize: 25, color: Colors.white))),
+                Container(
+                    margin: EdgeInsets.only(bottom: 10),
+                    padding: EdgeInsets.all(10),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.deepOrange)),
+                      child: Text("Confirm",
+                          style: TextStyle(color: Colors.white, fontSize: 25)),
+                    )),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildItem(FoodModel foodModel, BuildContext context) {
+    return Container(
+      height: 135,
+      child: Card(
+        elevation: 5,
+        shadowColor: Colors.blueGrey,
+        child: Container(
+          padding: EdgeInsets.only(top: 5, bottom: 5),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(2),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(5),
+                  child: Image.network(foodModel.images![0].imageUrl!,
+                      width: 150, height: 120, fit: BoxFit.fill),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 5),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 5),
+                        child: Text(foodModel.foodName!,
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(fontSize: 16)),
+                      ),
+                      Text(
+                          "Giá : " +
+                              NumberFormat("#,###", "en_US")
+                                  .format(foodModel.price) +
+                              " đ",
+                          style: TextStyle(fontSize: 12)),
+                      Row(
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              if (foodModel.quantity! > 1) {
+                                bloc.eventSink.add(CartEventOrderUpdate(
+                                    orderId: orderId,
+                                    foodId: foodModel.foodId!,
+                                    quantity: foodModel.quantity!.toInt() - 1));
+                              }
+                            },
+                            child: Text("-"),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 15),
+                            child: Text(foodModel.quantity.toString(),
+                                style: TextStyle(fontSize: 16)),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              bloc.eventSink.add(CartEventOrderUpdate(
+                                  orderId: orderId,
+                                  foodId: foodModel.foodId!,
+                                  quantity: foodModel.quantity!.toInt() + 1));
+                            },
+                            child: Text("+"),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
